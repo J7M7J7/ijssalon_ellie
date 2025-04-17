959632543181fd1691b836562dd4cc37652d0226
J.J Martina <Jim7.Dhar7.Gor7.Kha7@outlook.com>
2025-04-17 12:07:45 +0200

    functies toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,23 +1,29 @@
-prijzen = {
+from helper import decoreer
+
+def print_aanbieding():
+    prijzen = {
     "aardbei" : 3,
     "vanille" : 4,
     "chocolade" : 5
 }
 
-aanbieding = prijzen ["aardbei"] * 0.8
+    aanbieding = prijzen["aardbei"] * 0.8
 
-reclame_tekst = f"Vandaag in de aanbieding: Vanille-ijs, 1 Liter - slechts € {aanbieding}"
-print (reclame_tekst)
-reclame_tekst2 = reclame_tekst[:63]
-print(reclame_tekst2)
-reclame_tekst3 = reclame_tekst2.upper()
-print(reclame_tekst3)
-reclame_tekst4 = reclame_tekst3.split()
-print(reclame_tekst4)
-el = ["Vandaag in de aanbieding:", "Vanille-ijs","Liter","slechts"]
-print (el)
-for el in reclame_tekst4:
-    if len(el) > 4:
-     print (el.upper())    
-    else:
-        print(el.lower())
+    reclame_tekst = f"Vandaag in de aanbieding: Vanille-ijs, 1 Liter - slechts € {aanbieding}"
+    print (reclame_tekst)
+    reclame_tekst2 = reclame_tekst[:63]
+    print(reclame_tekst2)
+    reclame_tekst3 = reclame_tekst2.upper()
+    print(reclame_tekst3)
+    reclame_tekst4 = reclame_tekst3.split()
+    print(reclame_tekst4)
+    el = ["Vandaag in de aanbieding:", "Vanille-ijs","Liter","slechts"]
+    print (el)
+    for el in reclame_tekst4:
+        if len(el) > 4:
+            print (el.upper())
+        else:
+            print(el.lower())
+
+decoreer("aanbieding")
+print_aanbieding()
