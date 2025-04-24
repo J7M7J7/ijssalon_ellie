2659408ed2870e75cec521cad9f7653933afd80c
J.J Martina <Jim7.Dhar7.Gor7.Kha7@outlook.com>
2025-04-24 15:54:13 +0200

    Eerste versie van boekhouding en presentatie toegevoegd

--- helper.py
+++ helper.py
@@ -1,20 +1,3 @@
-def decoreer(tekst=""):
-    lengte = len(tekst) + 4
-    print()
-    print(lengte * "*")
-    print(f"* {tekst} *")
-    print(lengte * "*")
-    print()
-
-def fooi_pp(bedrag,personen):
-    bedrag_pp = bedrag/personen
-    return f"Het bedrag per persoon is {bedrag_pp} euro"
-
-b = int(input("Welk bedrag zit er in de fooienpot? "))
-p = int(input("Over hoeveel mensen moet de pot verdeeld worden? "))
-
-print(fooi_pp(b,p))
-
 def onderstreep (tekst=""):
         uit = []
         uit.append(tekst)
